aacb977a4950a54f505dbd6bdb185d0e46824408
ArxhentëKadriu <149197219+arxhentakadriu@users.noreply.github.com>
2026-05-17 23:28:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // gjithmonë përdor këtë port
+    port: 5173,
+    
   },
 });
