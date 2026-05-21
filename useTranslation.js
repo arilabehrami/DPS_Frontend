fc1266e326b0bccb59bea1b33e0c685206ae5d39
Rona <rona.maxhuni@student.uni-pr.edu>
2026-05-21 19:17:09 +0200

    Delete useTranslation.js

--- useTranslation.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { LanguageContext } from '../context/LanguageContext'
-
-export function useTranslation() {
-  const ctx = useContext(LanguageContext)
-  if (!ctx) throw new Error('useTranslation must be used within LanguageProvider')
-  return ctx
-}
